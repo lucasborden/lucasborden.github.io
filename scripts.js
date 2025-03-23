254d00e0e6396284bb18a9c9b74e229e7a170bda
lucasborden <lucas.f.borden@gmail.com>
2025-03-23 21:17:00 +0100

    fix folder paths

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Form submitted! (No backend yet)');
-  });
-  
+const links = document.querySelectorAll(".nav-links a");
+const currentPage = window.location.pathname.split("/").filter(Boolean).pop(); // get last part of path
+
+links.forEach(link => {
+  const linkPage = link.getAttribute("href").split("/").filter(Boolean).pop();
+  if (linkPage === currentPage || (currentPage === undefined && linkPage === "about")) {
+    link.classList.add("active");
+  }
+});
